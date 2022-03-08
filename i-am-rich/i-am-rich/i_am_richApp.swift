223c80ed25d861a731946b98e233dc5c5e2d3ee1
Aman Mishra <aman.nith@gmail.com>
2022-03-08 00:49:25 -0600

    Initial commit

---
+++ i-am-rich/i-am-rich/i_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_richApp.swift
+//  i-am-rich
+//
+//  Created by Aman Mishra on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct i_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
